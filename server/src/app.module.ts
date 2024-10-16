90fb94f25112f0600a9ec9e515a7ccfc41b6c652
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-10-16 19:10:27 +0100

    ~feat: Apply AtGuard guard globally

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { AtGuard } from './common';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
 	imports: [PrismaModule, AuthModule, UserModule],
 	controllers: [],
-	providers: [],
+	providers: [
+		{ provide: APP_GUARD, useClass: AtGuard }
+	],
 })
 export class AppModule {}
