9076c2ae1afdfd83c3e101c52dc7c807576255c0
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-10-16 19:10:03 +0100

    ~fix: decorator bug

--- server/src/common/decorators/get-current-user-id.decorator.ts
+++ server/src/common/decorators/get-current-user-id.decorator.ts
@@ -1,6 +1,6 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
-export const GetCurrentUserID = createParamDecorator((context: ExecutionContext): string => {
+export const GetCurrentUserID = createParamDecorator((_data: unknown, context: ExecutionContext): string => {
 	const request = context.switchToHttp().getRequest();
 	return request.user['sub']
 });
