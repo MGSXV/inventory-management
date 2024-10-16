dcd400d50429efb21f50e5d27e48be2454901a21
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-10-16 13:41:35 +0100

    ~restructuring the code

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from 'process';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);	
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
-	origin: env.CORS_ORIGIN,
-	credentials: true,
-	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    origin: env.CORS_ORIGIN,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
