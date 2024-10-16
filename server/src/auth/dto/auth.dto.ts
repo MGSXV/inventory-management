6fe8877b440e64156f2f832d006cf4e8b6a912c7
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-10-16 16:36:27 +0100

    ~feat: DTO error messages are costume

--- server/src/auth/dto/auth.dto.ts
+++ server/src/auth/dto/auth.dto.ts
@@ -2,11 +2,11 @@ import { IsAlphanumeric, IsNotEmpty, IsString, IsStrongPassword, MaxLength, MinL
 
 export class AuthDto {
 
-	@IsString()
-	@IsAlphanumeric()
-	@MinLength(4)
-	@MaxLength(20)
-	@IsNotEmpty()
+	@IsString({ message: 'SIGNUP.USERNAME_MUST_BE_STRING' })
+	@IsAlphanumeric("en-US", { message: 'SIGNUP.USERNAME_MUST_BE_ALPHANUMERIC' })
+	@MinLength(4, { message: 'SIGNUP.USERNAME_MIN_LENGTH' })
+	@MaxLength(20, { message: 'SIGNUP.USERNAME_MAX_LENGTH' })
+	@IsNotEmpty({ message: 'SIGNUP.USERNAME_REQUIRED' })
 	username: string;
 
 	@IsString()
@@ -16,16 +16,16 @@ export class AuthDto {
 		minUppercase: 1,
 		minNumbers: 1,
 		minSymbols: 1,
-	})
-	@MinLength(8)
-	@IsNotEmpty()
+	}, { message: 'SIGNUP.PASSWORD_WEAK' })
+	@MinLength(8, { message: 'SIGNUP.PASSWORD_MIN_LENGTH' })
+	@IsNotEmpty({ message: 'SIGNUP.PASSWORD_REQUIRED' })
 	password: string;
 
-	@IsString()
-	@IsNotEmpty()
+	@IsString({ message: 'SIGNUP.FIRST_NAME_MUST_BE_STRING' })
+	@IsNotEmpty({ message: 'SIGNUP.FIRST_NAME_REQUIRED' })
 	first_name: string;
 
-	@IsString()
-	@IsNotEmpty()
+	@IsString({ message: 'SIGNUP.LAST_NAME_MUST_BE_STRING' })
+	@IsNotEmpty({ message: 'SIGNUP.LAST_NAME_REQUIRED' })
 	last_name: string;
 }
--- server/src/user/dto/update_user.dto.ts
+++ server/src/user/dto/update_user.dto.ts
@@ -2,36 +2,29 @@ import { IsAlphanumeric, IsNotEmpty, IsNumber, IsString, IsStrongPassword, MaxLe
 
 export class UpdateUserDto {
 
-	@IsNotEmpty()
-	@IsNumber()
+	@IsNotEmpty({ message: 'USER.UPDATE.ID_REQUIRED' })
 	id: string;
 
-	@IsString()
-	@IsAlphanumeric()
-	@MinLength(4)
-	@MaxLength(20)
-	@IsNotEmpty()
+	@IsString({ message: 'USER.UPDATE.USERNAME_MUST_BE_STRING' })
+	@IsAlphanumeric("en-US", { message: 'USER.UPDATE.USERNAME_MUST_BE_ALPHANUMERIC' })
+	@MinLength(4, { message: 'USER.UPDATE.USERNAME_MIN_LENGTH' })
+	@MaxLength(20, { message: 'USER.UPDATE.USERNAME_MAX_LENGTH' })
 	username: string;
 
-	@IsString()
+	@IsString({ message: 'USER.UPDATE.PASSWORD_MUST_BE_STRING' })
 	@IsStrongPassword({
 		minLength: 8,
 		minLowercase: 1,
 		minUppercase: 1,
 		minNumbers: 1,
 		minSymbols: 1,
-	})
-	@MinLength(8)
-	@IsNotEmpty()
+	}, { message: 'USER.UPDATE.PASSWORD_WEAK' })
+	@MinLength(8, { message: 'USER.UPDATE.PASSWORD_MIN_LENGTH' })
 	password: string;
 
-	@IsString()
-	@IsNotEmpty()
+	@IsString({ message: 'USER.UPDATE.FIRST_NAME_MUST_BE_STRING' })
 	first_name: string;
 
-	@IsString()
-	@IsNotEmpty()
+	@IsString({ message: 'USER.UPDATE.LAST_NAME_MUST_BE_STRING' })
 	last_name: string;
-
-	refresh_token: string;
 }
