a8ca255d5744147ca2c8b2d9f4444bf3e5f27d3b
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-10-17 13:34:39 +0100

    ~add: default avatars

--- server/src/common/conf/dir.conf.ts
+++ server/src/common/conf/dir.conf.ts
@@ -4,3 +4,6 @@ export const STORAGE_DIR = join(process.cwd(), 'storage');
 export const IMG_DIR = join(STORAGE_DIR, 'images');
 export const DEPOT_IMG_DIR = join(IMG_DIR, 'depot');
 export const USER_IMG_DIR = join(IMG_DIR, 'user');
+
+export const DEFAULT_DEPOT_IMG = join(DEPOT_IMG_DIR, 'default.png');
+export const DEFAULT_USER_IMG = join(USER_IMG_DIR, 'default.png');
