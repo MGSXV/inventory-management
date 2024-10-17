f80ad1bf48d1221fca590093a6011840c3e05b16
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-10-17 13:14:53 +0100

    ~add: New error messages

---
+++ server/src/common/errors/depot.error.ts
@@ -0,0 +1,16 @@
+export const DEPOT = {
+	CREATE: {
+		NAME: {
+			MUST_BE_STRING: 'MUST_BE_STRING',
+			REQUIRED: 'REQUIRED',
+			MIN_LENGTH: 'MIN_LENGTH',
+			MAX_LENGTH: 'MAX_LENGTH',
+		},
+		DESCRIPTION: {
+			MUST_BE_STRING: 'MUST_BE_STRING',
+		},
+		IMAGE_URL: {
+			MUST_BE_URL: 'MUST_BE_URL',
+		},
+	}
+}
--- server/src/common/errors/index.ts
+++ server/src/common/errors/index.ts
@@ -1,3 +1,4 @@
 export * from './signup.error';
 export * from './signin.error';
 export * from './auth.error';
+export * from './depot.error';
