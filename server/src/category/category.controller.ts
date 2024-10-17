c3a482132abb28ea830e14b396b5f2cbdb2c3d4b
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-10-17 21:31:59 +0100

    ~feat: category entity is done

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,34 +1,68 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpStatus } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { GetCurrentUserID } from 'src/common/decorators';
 
 @Controller('category')
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
+	constructor(private readonly categoryService: CategoryService) {}
 
-  @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoryService.create(createCategoryDto);
-  }
+	@Post(':depot_id')
+	async create(@Param('depot_id') depot_id: string, @Body() createCategoryDto: CreateCategoryDto, @GetCurrentUserID() userID: string) {
+		try {
+			return await this.categoryService.create(depot_id, userID, createCategoryDto);
+		} catch (error) {
+			throw new HttpException({
+				status: HttpStatus.BAD_REQUEST,
+				error: error.message
+			}, HttpStatus.BAD_REQUEST);
+		}
+	}
 
-  @Get()
-  findAll() {
-    return this.categoryService.findAll();
-  }
+	@Get(':depot_id')
+	async findAll(@Param('depot_id') depot_id: string, @GetCurrentUserID() userID: string) {
+		try {
+			return await this.categoryService.findAll(userID, depot_id);
+		} catch (error) {
+			throw new HttpException({
+				status: HttpStatus.BAD_REQUEST,
+				error: error.message
+			}, HttpStatus.BAD_REQUEST);
+		}
+	}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoryService.findOne(+id);
-  }
+	@Get(':depot_id/:id')
+	async findOne(@Param('id') id: string, @Param('depot_id') depot_id: string, @GetCurrentUserID() userID: string) {
+		try {
+			return await this.categoryService.findOne(id, depot_id, userID);
+		} catch (error) {
+			throw new HttpException({
+				status: HttpStatus.BAD_REQUEST,
+				error: error.message
+			}, HttpStatus.BAD_REQUEST);
+		}
+	}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return this.categoryService.update(+id, updateCategoryDto);
-  }
+	@Get(':depot_id/:id/children')
+	async findChildren(@Param('id') id: string, @Param('depot_id') depot_id: string, @GetCurrentUserID() userID: string) {
+		try {
+			return await this.categoryService.findChildren(id, depot_id, userID);
+		} catch (error) {
+			throw new HttpException({
+				status: HttpStatus.BAD_REQUEST,
+				error: error.message
+			}, HttpStatus.BAD_REQUEST);
+		}
+	}
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoryService.remove(+id);
-  }
+	@Patch(':depot_id/:id')
+	async update(@Param('id') id: string, @Param('depot_id') depot_id: string, @GetCurrentUserID() user_id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
+		return await this.categoryService.update(id, depot_id, user_id, updateCategoryDto);
+	}
+
+	@Delete(':depot_id/:id')
+	async remove(@Param('id') id: string, @Param('depot_id') depot_id: string, @GetCurrentUserID() user_id: string) {
+		return this.categoryService.remove(id, depot_id, user_id);
+	}
 }
