96f0e9141b96e19ca748656b96159eabcd5c9c25
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-10-18 22:58:11 +0100

    ~feat: add types and errors

--- client/src/config/errors/errors.ts
+++ client/src/config/errors/errors.ts
@@ -10,6 +10,17 @@ export const ERRORS = {
 	'SIGNIN.PASSWORD.MIN_LENGTH': 'Password is too short',
 	'SIGNIN.PASSWORD.REQUIRED': 'Password is required',
 	'SIGNIN.PASSWORD.INVALID': 'Password is invalid',
+	'SIGNUP.USERNAME.REQUIRED': 'Username is required',
+	'SIGNUP.USERNAME.MIN_LENGTH': 'Username is too short',
+	'SIGNUP.USERNAME.MUST_BE_ALPHANUMERIC': 'Username must be alphanumeric',
+	'SIGNUP.PASSWORD.REQUIRED': 'Password is required',
+	'SIGNUP.PASSWORD.MIN_LENGTH': 'Password is too short',
+	'SIGNUP.PASSWORD.WEAK': 'Password is weak',
+	'SIGNUP.FIRST_NAME.REQUIRED': 'First name is required',
+	'SIGNUP.FIRST_NAME.MUST_BE_STRING': 'First name must be a string',
+	'SIGNUP.LAST_NAME.REQUIRED': 'Last name is required',
+	'SIGNUP.LAST_NAME.MUST_BE_STRING': 'Last name must be a string',
+	'SIGNUP.USERNAME.ALREADY_EXISTS': 'Username already exists',
 }
 
 export function getErrorMessage(data: any) {
