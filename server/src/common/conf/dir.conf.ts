3faba5f57e42f7b95833d895d9c7254b15ac0fc9
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-10-22 15:41:11 +0100

    ~fix: Dir config

--- server/src/common/conf/dir.conf.ts
+++ server/src/common/conf/dir.conf.ts
@@ -1,9 +1,11 @@
 import { join } from "path";
 
-export const STORAGE_DIR = join(process.cwd(), 'storage');
-export const IMG_DIR = join(STORAGE_DIR, 'images');
-export const DEPOT_IMG_DIR = join(IMG_DIR, 'depot');
-export const USER_IMG_DIR = join(IMG_DIR, 'user');
+export const DEPOT_STORAGE = 'depot';
+export const USER_STORAGE = 'user';
+export const IMGS_STORAGE = 'images';
+export const IMG_DIR = join(IMGS_STORAGE);
+export const DEPOT_IMG_DIR = join(IMG_DIR, DEPOT_STORAGE);
+export const USER_IMG_DIR = join(IMG_DIR, USER_STORAGE);
 
 export const DEFAULT_DEPOT_IMG = join(DEPOT_IMG_DIR, 'default.png');
 export const DEFAULT_USER_IMG = join(USER_IMG_DIR, 'default.png');
