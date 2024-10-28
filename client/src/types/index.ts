da3de424f1006c2b42baef87f191742fb5456801
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-10-28 16:03:59 +0100

    refactor: Add supplier context and types
    This commit adds the supplier context and types to the project. It includes the addition of the 'supplier-contxt.d.ts' file, which defines the ISupplier interface and TSupplierContext type. These additions allow for the management of suppliers within the application.

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,3 +1,4 @@
 export * from './current_user';
 export * from './auth-context';
 export * from './depot-context';
+export * from './supplier-contxt'
---
+++ client/src/types/supplier-contxt.d.ts
@@ -0,0 +1,14 @@
+export interface ISupplier {
+	id: string
+	name: string
+	description: string
+	image_url: string
+	created_at: string
+	updated_at: string
+	created_by_id: string
+}
+
+export type TSupplierContext = {
+	suppliers: ISupplier[],
+	setSuppliers: React.Dispatch<React.SetStateAction<ISupplier[]>>,
+}
