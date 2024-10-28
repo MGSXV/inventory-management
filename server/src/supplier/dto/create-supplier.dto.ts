ea788ce625f9d7ee0a8e08efce5765313c30bfdd
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-10-28 17:49:06 +0100

    refactor: Update CreateSupplierDto with validation and error messages

---
+++ server/src/supplier/dto/create-supplier.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsOptional, IsString, IsUrl, MaxLength, MinLength } from "class-validator";
+import { SUPPLIER } from "src/common/errors";
+
+export class CreateSupplierDto {
+	@IsString({ message: `DEPOT.CREATE.NAME.${SUPPLIER.CREATE.NAME.MUST_BE_STRING}` })
+	@IsNotEmpty({ message: `SUPPLIER.CREATE.NAME.${SUPPLIER.CREATE.NAME.REQUIRED}` })
+	@MinLength(4, { message: `SUPPLIER.CREATE.NAME.${SUPPLIER.CREATE.NAME.MIN_LENGTH}` })
+	@MaxLength(20, { message: `SUPPLIER.CREATE.NAME.${SUPPLIER.CREATE.NAME.MAX_LENGTH}` })
+	name: string;
+
+	@IsOptional()
+	@IsString({ message: `SUPPLIER.CREATE.DESCRIPTION.${SUPPLIER.CREATE.DESCRIPTION.MUST_BE_STRING}` })
+	@MaxLength(200, { message: `SUPPLIER.CREATE.DESCRIPTION.${SUPPLIER.CREATE.DESCRIPTION.MAX_LENGTH}` })
+	description?: string;
+
+	@IsOptional()
+	file?: string;
+}
