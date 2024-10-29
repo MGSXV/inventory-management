d5e46c7673ca89484afc6617772d253bae4ae3c8
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-10-29 15:58:22 +0100

    refactor: Add SupplierProvider to LayoutAuth component

--- client/src/components/common/LayoutAuth.tsx
+++ client/src/components/common/LayoutAuth.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom"
 import { SidebarProvider } from "@/components/ui/sidebar"
-import { DepotProvider } from "@/context"
+import { DepotProvider, SupplierProvider } from "@/context"
 import AuthOutlet from "./AuthOutlet"
 
 const LayoutAuth = () => {
 	return (
 		<SidebarProvider>
 			<DepotProvider>
-				<AuthOutlet>
-					<Outlet />
-				</AuthOutlet>
+				<SupplierProvider>
+					<AuthOutlet>
+						<Outlet />
+					</AuthOutlet>
+				</SupplierProvider>
 			</DepotProvider>
 		</SidebarProvider>
 	)
