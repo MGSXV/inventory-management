0a1f842dfa65338041f8b8f995e8c52665031c1a
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-10-30 15:36:36 +0100

    feat: Add CategoryController and CategoryService with create, findAll, findOne, update, and remove methods

---
+++ server/src/category/category.controller.ts
@@ -0,0 +1,69 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException, HttpException, HttpStatus } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { FileUploadService } from 'src/file-upload/file-upload.service';
+import { GetCurrentUserID } from 'src/common/decorators';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CATEGORY } from 'src/common/errors';
+import { CATEGORY_IMG_DIR } from 'src/common/conf';
+
+const MAX_FILE_SIZE = 1024 * 1024 * 10;
+
+@Controller('category')
+export class CategoryController {
+	constructor(
+		private readonly categoryService: CategoryService,
+		private readonly uploadFileServive: FileUploadService
+	) {}
+
+	@Post()
+	@UseInterceptors(FileInterceptor('file', {
+		limits: { fileSize: MAX_FILE_SIZE },
+		fileFilter: (req, file, cb) => {
+			if (file.mimetype.match(/\/(jpg|jpeg|png)$/)) {
+				cb(null, true);
+			} else {
+				cb(new BadRequestException(`SUPPLIER.CREATE.IMAGE_URL.${CATEGORY.CREATE.IMAGE_URL.INVALID_FILE_TYPE}`), false);
+			}
+		}
+	}))
+	async create(
+		@Body() createCategoryDto: CreateCategoryDto,
+		@GetCurrentUserID() userID: string,
+		@UploadedFile() file: Express.Multer.File
+	) {
+		try {
+			if (file)
+				createCategoryDto.file = await this.uploadFileServive.uploadFile(file, CATEGORY_IMG_DIR);
+			else
+				createCategoryDto.file = undefined;
+			return await this.categoryService.create(createCategoryDto, userID);
+		} catch (error) {
+			throw new HttpException({
+				status: HttpStatus.BAD_REQUEST,
+				error: error.message,
+			}, HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@Get()
+	findAll() {
+		return this.categoryService.findAll();
+	}
+
+	@Get(':id')
+	findOne(@Param('id') id: string) {
+		return this.categoryService.findOne(+id);
+	}
+
+	@Patch(':id')
+	update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
+		return this.categoryService.update(+id, updateCategoryDto);
+	}
+
+	@Delete(':id')
+	remove(@Param('id') id: string) {
+		return this.categoryService.remove(+id);
+	}
+}
---
+++ server/src/category/category.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { EState } from '@prisma/client';
+import { CATEGORY } from 'src/common/errors';
+
+@Injectable()
+export class CategoryService {
+
+	constructor(private prisma: PrismaService) {}
+
+	async create(createCategoryDto: CreateCategoryDto, userID: string) {
+
+		try {
+			if (createCategoryDto.parent) {
+				const is_second_level = await this.hasParentCategory(createCategoryDto.parent);
+				if (is_second_level) {
+					throw new Error('CATEGORY.CREATE.PARENT_CATEGORY');
+				}
+			}
+			const category = this.prisma.category.create({
+				data: {
+					name: createCategoryDto.name,
+					description: createCategoryDto.description,
+					image_url: createCategoryDto.file || null,
+					parentCategoryId: createCategoryDto.parent || null,
+					created_by_id: userID,
+					state: EState.ACTIVE
+				},
+				select: {
+					name: true,
+					description: true,
+					image_url: true,
+					parentCategoryId: true,
+					created_by_id: true,
+					parentCategory: {
+						select: {
+							name: true,
+							description: true,
+							image_url: true,
+							parentCategoryId: true,
+							created_by_id: true,
+						}
+					}
+				}
+			}).then(category => category)
+				.catch(() => {throw new Error(`CATEGORY.CREATE.${CATEGORY.CREATE.GENERAL}`)});
+			return category;
+		} catch (error) {
+			throw new Error(error.message);
+		}
+
+		return 'This action adds a new category';
+	}
+
+	findAll() {
+		return `This action returns all category`;
+	}
+
+	findOne(id: number) {
+		return `This action returns a #${id} category`;
+	}
+
+	update(id: number, updateCategoryDto: UpdateCategoryDto) {
+		return `This action updates a #${id} category`;
+	}
+
+	remove(id: number) {
+		return `This action removes a #${id} category`;
+	}
+
+	private async hasParentCategory(id: string) {
+		const category = await this.prisma.category.findFirst({
+			where: {
+				id: id
+			}
+		});
+		return category
+	}
+}
