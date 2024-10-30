bb411b648fd015a1577390c4ef5561437867e0d1
Soufiane Elkhamlichi <05soufiane@gmail.com>
2024-10-30 23:00:32 +0100

    refactor: Add error handling to category deletion in DeleteCategory component

--- client/src/components/category/delete-category.tsx
+++ client/src/components/category/delete-category.tsx
@@ -1,13 +1,37 @@
 import { Dialog, DialogHeader, DialogContent, DialogDescription, DialogTitle, DialogFooter } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 import { useState } from "react"
+import { useAxiosPrivate, useErrorHandler } from "@/hooks"
+import { useCategory } from "@/context"
 
 export const DeleteCategory = ({ isOpen, onOpenChange, id, parent_id }:
 	{ isOpen: boolean, id: string, parent_id: string, onOpenChange: (open: boolean) => void }) => {
 
 	const [isLoading, setIsLoading] = useState(false)
+	const { categories, setCategories } = useCategory()
+	const axios = useAxiosPrivate()
+	const errorHandler = useErrorHandler()
+
 	const handleDelete = () => {
 		setIsLoading(true)
+		axios.delete(`/category/${id}`).then(response => {
+			if (response.status === 200) {
+				if (parent_id !== '') {
+					const newCategories = categories.map(category => {
+						if (category.childCategories) {
+							category.childCategories = category.childCategories.filter(child => child.id !== id)
+						}
+						return category
+					})
+					setCategories(newCategories)
+				} else {
+					setCategories(categories.filter(category => category.id !== id))
+				}
+			}
+		}).catch(error => errorHandler(error)).finally(() => {
+			setIsLoading(false)
+			onOpenChange(false)
+		})
 	}
 
 	return (
